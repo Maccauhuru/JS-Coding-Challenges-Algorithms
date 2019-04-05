f31b2b3fa9077515c6d3e713d3664443a940c88b
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2019-04-04 20:37:44 -0500

    string reversal methods

---
+++ JS-SG-Algocasts/reversestring/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split("").reduce((prev, curr) => curr + prev, "");
+}
+
+module.exports = reverse;
+
+//Solution 1
+// function reverse(str) {
+//     var reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+//Solution 2
+// function reverse(str) {
+//     let reversed = "";
+//     for (let char of str) {
+//         reversed = char + reversed;
+//     }
+//     return reversed;
+// }
---
+++ JS-SG-Algocasts/reversestring/test.js
@@ -0,0 +1,17 @@
+const reverse = require("./index.js");
+
+test("Reverse function exists", () => {
+  expect(reverse).toBeDefined();
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("abcd")).toEqual("dcba");
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("  abcd")).toEqual("dcba  ");
+});
+
+test("Reverse reverses a string", () => {
+  expect(reverse("remember")).toEqual("rebmemer");
+});
