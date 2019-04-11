d9bd45914c43d4af7487baa8c360f1caa7f0701b
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2019-04-11 11:02:19 -0500

    alt solution to palindrome

--- JS-SG-Algocasts/palindrome/palindrome.js
+++ JS-SG-Algocasts/palindrome/palindrome.js
@@ -1,11 +1,17 @@
 function palindrome(str) {
-  return str.split("").reduce((prev, curr) => curr + prev, "") === str
-    ? true
-    : false;
+  return str
+    .split("")
+    .every((val, i) => (val === str[str.length - i - 1] ? true : false));
 }
 
 module.exports = palindrome;
 
 // function palindrome(str) {
 //     return (str === str.split('').reverse().join('')) ? true : false;
+// }
+
+// function palindrome(str) {
+//   return str.split("").reduce((prev, curr) => curr + prev, "") === str
+//     ? true
+//     : false;
 // }
